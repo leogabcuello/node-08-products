39f16e7ffa42dac28c51fe776f9c000fb902f0e8
leonardo <leogabcuello@gmail.comm>
2021-05-21 02:05:34 -0300

    add Validation in POST

--- src/middlewares/users/index.js
+++ src/middlewares/users/index.js
@@ -1,6 +1,7 @@
 const {check, validationResult } = require('express-validator');
 const AppError = require ('../../errors/appError');
-const { findByEmail } = require('../../')
+const { findByEmail } = require('../../services/userService');
+const { ROLE } = require('../../constants'); 
  
 const _nameRequeride = check('name', 'Name Requeride').not().isEmpty();
 const _lastNameRequeride = check('lastName', 'LastName Requeride').not().isEmpty();
@@ -8,9 +9,21 @@ const _emailRequeride = check('email', 'email Requeride').not().isEmpty();
 const _emailValid = check('email', 'email is invalid').isEmail();
 const _emailExist = check('email').custom(
     async (email = '') => {
-        
+        const userFound = await findByEmail(email);
+        if (userFound){
+            throw new AppError('Emmail already exist in db', 400);
+        }
     }
 )
+const _passwordRequeride = check('password', 'password is Requeride').not().isEmpty();
+const _roleValid = check('role').optional().custom(
+    async (role = '') => {
+        if (!ROLE.include(role)){
+            throw new AppError('Role invalid', 400);
+        }
+    }
+)
+const _dateValid = check('birthDate').isDate('MM-DD-AAAA');
 
 const _validationResult =  (req, res, next) => {
     const errors = validationResult(req);
@@ -24,6 +37,10 @@ const postReuestValidation = [
     _lastNameRequeride,
     _emailRequeride,
     _emailValid,
+    _emailExist,
+    _passwordRequeride,
+    _roleValid,
+    _dateValid,
     _validationResult
 ]
 
